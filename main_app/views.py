ce554890d9af6ab5d7489e47b2d44e2eac887bc9
samantha frangi <sefrangi13@gmail.com>
2022-03-15 19:10:08 -0400

    wip: successfully create routes for items, updates to views

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Product, Color
 # Create your views here.
@@ -9,18 +9,17 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-def all_products(request):
-  return render(request, 'product_list.html')
-
 def products_index_by_tag(request, product_tag):
-  if product_tag == 'Vegan':
+  if product_tag == 'vegan':
       products_list = Product.objects.filter(tags__icontains=product_tag)
   elif product_tag == 'cruelty-free' or product_tag == 'cruelty free':
       sanitized_product_tag = product_tag.replace('-', ' ')
       products_list = Product.objects.filter(tags__icontains=sanitized_product_tag)
   elif product_tag == 'natural':
       products_list = Product.objects.filter(tags__icontains=product_tag)
-      return render(request, 'products_index.html', {'product': products_list})
+  else: 
+    return redirect('home')
+  return render(request, 'products_index.html', {'product': products_list})
 
 def products_detail(request, id):
   product = Product.objects.get(id=id)
