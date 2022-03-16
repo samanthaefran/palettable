110972c8b19f5e677b4d52d961c150d03afa8b66
Samantha Elaine <85855171+ybsamantha@users.noreply.github.com>
2022-03-15 20:33:41 -0400

    fixes views and home

--- main_app/views.py
+++ main_app/views.py
@@ -9,7 +9,7 @@ def about(request):
   return render(request, 'about.html')
 
 def products_index_by_tag(request, product_tag):
-  if product_tag == 'vegan':
+  if product_tag == 'Vegan':
       products_list = Product.objects.filter(tags__icontains=product_tag)
   elif product_tag == 'cruelty-free' or product_tag == 'cruelty free':
       sanitized_product_tag = product_tag.replace('-', ' ')
