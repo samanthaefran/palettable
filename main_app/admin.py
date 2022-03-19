f9263564079b89d280261638ca881aad04adc4db
Samantha Elaine <85855171+ybsamantha@users.noreply.github.com>
2022-03-19 16:29:31 -0400

    wip: updates admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Color
-
+from .models import Product, Color, Looks
 # Register your models here.
 admin.site.register(Product)
 
 admin.site.register(Color)
+
+admin.site.register(Looks)
