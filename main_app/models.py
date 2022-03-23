d5b38155444dee1dd11ed4c1365b69549b12324c
Samantha Elaine <85855171+ybsamantha@users.noreply.github.com>
2022-03-23 00:17:25 -0400

    wip: updates models

--- main_app/models.py
+++ main_app/models.py
@@ -16,10 +16,6 @@ class Product(models.Model):
     users = models.ManyToManyField(User)
     favorites = models.ManyToManyField(User, related_name='favorite', default=None, blank=True)
 
-    class Meta:
-        managed = False
-        db_table = 'product'
-
     def __str__(self):
         return f'{self.product_name} by {self.brand}'
 
@@ -28,10 +24,6 @@ class Color(models.Model):
     hex_value = models.CharField(max_length=7, blank=True, null=True)
     colour_name = models.CharField(max_length=150, blank=True, null=True)
     color_id = models.AutoField(primary_key=True)
-
-    class Meta:
-        managed = False
-        db_table = 'color'
 
     def __str__(self):
         return f'{self.hex_value} {self.colour_name}'
