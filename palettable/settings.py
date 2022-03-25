762f683bd786d9fae35a5d29425981aa07ecff02
Samantha Elaine <85855171+ybsamantha@users.noreply.github.com>
2022-03-24 22:13:46 -0400

    removes database

--- palettable/settings.py
+++ palettable/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'palettable.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'palettable',
+        'NAME': '',
     }
 }
 
