7cc54179dacb0a15db95ea1f8174931bee332918
Samantha Elaine <85855171+ybsamantha@users.noreply.github.com>
2022-03-24 22:27:44 -0400

    updates models

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here:
+
+
 class Product(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
     brand = models.CharField(max_length=150)
@@ -14,10 +16,16 @@ class Product(models.Model):
     product_description = models.CharField(max_length=4000)
     product_type = models.CharField(max_length=150)
     users = models.ManyToManyField(User)
-    favorites = models.ManyToManyField(User, related_name='favorite', default=None, blank=True)
+    favorites = models.ManyToManyField(
+        User, related_name='favorite', default=None, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'product'
 
     def __str__(self):
         return f'{self.product_name} by {self.brand}'
+
 
 class Color(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -25,15 +33,19 @@ class Color(models.Model):
     colour_name = models.CharField(max_length=150, blank=True, null=True)
     color_id = models.AutoField(primary_key=True)
 
+    class Meta:
+        managed = False
+        db_table = 'color'
+
     def __str__(self):
         return f'{self.hex_value} {self.colour_name}'
+
 
 class Look(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     products = models.ManyToManyField(Product)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.name
-    
